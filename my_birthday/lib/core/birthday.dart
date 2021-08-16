f961d3d8fd3461db23058ee2853145e47010475c
MarceloJSCesar <jsdevmarcelo@gmail.com>
2021-08-15 20:42:42 -0400

    project started

---
+++ my_birthday/lib/core/birthday.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import './views/home/home_view.dart';
+
+class Birthday extends StatelessWidget {
+  @override
+  MaterialApp build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeView(),
+    );
+  }
+}
---
+++ my_birthday/lib/core/views/home/home_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Scaffold build(BuildContext context) {
+    return Scaffold();
+  }
+}
